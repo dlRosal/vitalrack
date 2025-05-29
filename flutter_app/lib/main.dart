20a417464c21b0bf79aafebc8db712fbc0b146b0
Álvaro del Rosal <78663498+dlRosal@users.noreply.github.com>
2025-05-29 16:48:54 +0200

    Auth
    Auth

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -20,7 +20,7 @@ class VitalrackApp extends ConsumerWidget {
     return MaterialApp(
       title: 'Vitalrack',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const NutritionScreen(),
+      home: const AuthWrapper(),
       routes: {
         // Rutas protegidas tras login
         '/nutrition': (_) => const NutritionScreen(),
