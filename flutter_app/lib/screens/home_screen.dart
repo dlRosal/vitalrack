6934dc15f48882a071df785ca6de9b9d1efaa437
Álvaro del Rosal <78663498+dlRosal@users.noreply.github.com>
2025-06-03 14:43:31 +0200

    Quitando deprecated en home
    Quitando deprecated

--- flutter_app/lib/screens/home_screen.dart
+++ flutter_app/lib/screens/home_screen.dart
@@ -14,14 +14,14 @@ class HomeScreen extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               SizedBox(
-                height: 250, // o prueba 200 si quieres aún más grande
+                height: 250,
                 child: Image.asset('assets/logosinfondo.png'),
               ),
 
               const SizedBox(height: 32),
-              Text(
+              const Text(
                 'Bienvenido a Vitalrack',
-                style: const TextStyle(
+                style:  TextStyle(
                   fontSize: 28,
                   fontWeight: FontWeight.bold,
                   color: Color(0xFFE0E0E0),
